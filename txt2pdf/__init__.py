e4102c32210124e60e2112ff166501ee6648068e
c4ffein <c4ffein>
2021-08-02 00:50:30 +0200

    0.6.6 : switched from docopt to argparse

--- txt2pdf/__init__.py
+++ txt2pdf/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 __title__ = "txt2pdf"
-__version__ = "0.6.5"
+__version__ = "0.6.6"
 __author__ = "Julien Maupetit & c4ffein"
 __license__ = "MIT"
 __copyright__ = "Copyright 2013-2021 Julien Maupetit & c4ffein"
